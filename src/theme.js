86696bdeab6088c3abdcba5290140f613579e5ad
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2020-03-02 13:33:04 -0500

    adds white and black shades

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,15 @@
 const theme = {
-    testColor: "#DD4422"
+    colors: {
+        primary: "#210D1B",
+        primaryLight: "#725752",
+
+        secondary: "#584D3D",
+        secondaryLight: "#E5BE9E",
+        secondaryDark:"#F5E3E0",
+
+        white: "#FFFFFF",
+        black: "#111111"
+    }
 }
 
 export default theme

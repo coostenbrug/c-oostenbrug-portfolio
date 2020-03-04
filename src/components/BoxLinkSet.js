5d7c9f3874e0dc7d632b1a933b7d4e6e17295067
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2020-03-04 12:01:51 -0500

    fixed missing export on BoxLinkSet

---
+++ src/components/BoxLinkSet.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+const BoxLinkSet = styled.div({
+    display: "flex"
+})
+
+export default BoxLinkSet
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button"
 export { default as BoxLink } from "./BoxLink"
+export { default as BoxLinkSet } from "./BoxLinkSet"
 export { default as Tag } from "./Tag"
 export { default as TagSet } from "./TagSet"
